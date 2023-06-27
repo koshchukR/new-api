cb1a4391303526a57c77e10af76041fe2d80a534
VY <admin@admins-Mac-mini.local>
2023-06-27 13:41:46 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from "@nestjs/config";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { loggerMiddleware } from "./middleware/logging.middleware";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.use(loggerMiddleware);
     const configService = app.get(ConfigService);
     const PORT = configService.get('PORT');
+
+
+    const config = new DocumentBuilder()
+      .setTitle('x24')
+      .setVersion('1.0')
+      .addTag('x24')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(PORT, () => {
     });
