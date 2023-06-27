216b15f6e5074bca25499b140e182d3315a71790
VY <admin@admins-Mac-mini.local>
2023-06-27 15:06:01 +0300

    update run script and add swagget

--- src/modules/languages/contracts/models/languages.interface.ts
+++ src/modules/languages/contracts/models/languages.interface.ts
@@ -1,14 +1,14 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LanguagesInterface {
-  @ApiProperty({type: Number, required: false})
-  _id?: number
+  @ApiProperty({ type: Number, required: false })
+  _id?: number;
   @ApiProperty()
-  language: string
+  language: string;
 
   @ApiProperty()
-  lang_short: string
+  lang_short: string;
 
-  @ApiProperty({type: Boolean})
-  can_delete?: boolean
+  @ApiProperty({ type: Boolean })
+  can_delete?: boolean;
 }
